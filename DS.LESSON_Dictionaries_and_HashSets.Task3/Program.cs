f9b129e707c16f01ef293d9dfbac62843cd4f1fa
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-31 13:44:02 +0200

    Добавьте файлы проекта.

---
+++ DS.LESSON_Dictionaries_and_HashSets.Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace DS.LESSON_Dictionaries_and_HashSets.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\1392659\Desktop\words.txt", text;
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine("This is the TEXT. Text, text, text – THIS TEXT! Is this the text?");
+            };
+            using (StreamReader sr = new StreamReader(path, true))
+            {
+                text = (sr.ReadLine()).ToLower();
+            };
+            text = text.Replace(".", "").Replace(",", "").Replace("–", "").Replace("?", "").Replace("!", "");
+            string[] list = text.Split(" ");
+            int[] count = new int[4];
+            int a = 0;
+            while(a < list.Length)
+            {
+                switch (list[a])
+                {
+                    case ("this"):
+                        count[0]++;
+                        break;
+                    case ("is"):
+                        count[1]++;
+                        break;
+                    case ("the"):
+                        count[2]++;
+                        break;
+                    case ("text"):
+                        count[3]++;
+                        break;
+                }
+                a++;
+            }
+            Console.WriteLine("this -> " + count[0]);
+            Console.WriteLine("is -> " + count[1]);
+            Console.WriteLine("the -> " + count[2]);
+            Console.WriteLine("text -> " + count[3]);
+        }
+    }
+}
